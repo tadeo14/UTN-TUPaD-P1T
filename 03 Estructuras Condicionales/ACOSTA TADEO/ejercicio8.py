360628bf6e2ea3e3aed70e34df2cbba69af01585
tadeo <tadeoacosta14@gmail.com>
2025-06-13 16:23:14 -0300

    comence el ejercicio 8

--- 03 Estructuras Condicionales/ACOSTA TADEO/ejercicio8.py
+++ 03 Estructuras Condicionales/ACOSTA TADEO/ejercicio8.py
@@ -8,3 +8,14 @@ dependiendo de la opción que desee:
 El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el
 usuario e imprimir el resultado por pantalla. Nota: investigue uso de las funciones upper(),
 lower() y title() de Python para convertir entre mayúsculas y minúsculas"""
+
+while True:
+
+    nombre = input("Coloque su nombre: ")
+    opcion = input(
+    "Seleccione una opción:\n"
+    "1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.\n"
+    "2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.\n"
+    "3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.\n"
+    "Ingrese 1, 2 o 3: "
+)
